9a7346e0cc0507e671dcaa7ba096879475f15ce3
Marcos Lira <liram1995@gmail.com>
2019-12-06 11:29:02 -0600

    Completed MVP

---
+++ routers/actionRouter.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const actionDb = require('../data/helpers/actionModel');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    actionDb.get(req.params.id)
+        .then(thing => {
+            res.status(200).json(thing);
+        })
+        .catch(err => {
+            res.status(500).json({ message: " error handling request..." });
+        })
+});
+
+
+router.post('/', (req, res) => {
+    actionDb.insert(req.body)
+        .then(thing => {
+            res.status(201).json(thing);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "there was an error with the request..." })
+        })
+});
+
+
+router.put('/:id', (req, res) => {
+    actionDb.update(req.params.id, req.body)
+        .then(thing => {
+            if (thing) {
+                res.status(200).json(thing)
+            } else {
+                res.status(404).json({ message: "The action could not be found" })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "there was an error with the request..." })
+        })
+});
+
+
+router.delete('/:id', (req, res) => {
+    actionDb.remove(req.params.id)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: "Action has been deleted..." })
+            } else {
+                res.status(404).json({ message: "could not find action" })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "there was an error with the request..." })
+        })
+})
+
+
+module.exports = router;
---
+++ routers/projectRouter.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const projectDb = require('../data/helpers/projectModel');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    projectDb.get(req.params.id)
+        .then(thing => {
+            res.status(200).json(thing);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: " there was an error with request" })
+        })
+});
+
+
+router.post('/', (req, res) => {
+    projectDb.insert(req.body)
+        .then(thing => {
+            res.status(201).json(thing);
+        })
+        .catch(err => {
+            console.log("There was an error with request...")
+            res.status(500).json({ message: "Error posting data..." })
+        })
+});
+
+
+router.put('/:id', (req, res) => {
+    projectDb.update(req.params.id, req.body)
+        .then(thing => {
+            if (thing) {
+                res.status(200).json(thing)
+            } else {
+                res.status(404).json({ message: "this project could not be found..." })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "Error updating project..." })
+        })
+});
+
+
+router.delete('/:id', (req, res) => {
+    projectDb.remove(req.params.id)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: "project was successfully deleted!!" })
+            } else {
+                res.status(404).json({ message: "Project could not be found" })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "error deleting project... " })
+        })
+});
+
+
+module.exports = router;
